7e2ce2b7c0feb98dd5a2a8ad382a74f8d946f7e7
YusukeYU <gustavooliveirapontes@gmail.com>
2021-03-09 22:57:08 -0300

    Ajustes front-end tarefas

--- Public/js/task.js
+++ Public/js/task.js
@@ -1,10 +1,26 @@
-
-$( "#dateNewTask" ).pickadate();
-$( "#timeNewTask" ).pickatime({
-    format: 'M!arc!ado em h:i',
+$("#dateNewTask").pickadate()
+$("#timeNewTask").pickatime({
+    format: 'M!arc!ado em H:i',
     formatLabel: 'H:i',
     formatSubmit: 'H:i'
 });
+$('#imgCalendar').on('click', function (e) {
+    e.preventDefault();
+    $("#dateNewTask").trigger('click')
+})
+$('#imgClock').on('click', function (e) {
+    e.preventDefault();
+    $("#timeNewTask").trigger('click')
+})
+function changeElement(id) {
+    $('.my-loader').css('display', 'flex')
+    var elements = ['#tableAllTasks', '#receiveFormNewTask'];
+    var element = "#" + id;
+    elements.forEach(function (item, indice, array) {
+        $(item).hide();
+    });
+    $(element).show();
+    $('.my-loader').hide();
+}
 
 
-  
